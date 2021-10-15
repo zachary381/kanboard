cc4acfab3c0fc411de66185d48af22246e21e531
zachary381 <54789347+zachary381@users.noreply.github.com>
2021-10-15 17:02:18 +0800

    Update config.php

--- config.php
+++ config.php
@@ -19,7 +19,8 @@ define('LOG_DRIVER', 'system');
 define('LOG_FILE', DATA_DIR.DIRECTORY_SEPARATOR.'debug.log');
 
 // Plugins directory
-define('PLUGINS_DIR', __DIR__.DIRECTORY_SEPARATOR.'plugins');
+//define('PLUGINS_DIR', __DIR__.DIRECTORY_SEPARATOR.'plugins');
+define('PLUGINS_DIR', 'data/plugins');
 
 // Plugins directory URL
 define('PLUGIN_API_URL', 'https://kanboard.org/plugins.json');
